c49091ac9d8ecbfae324acddd7a2305122e77bc9
Siddhi Panchal <siddhipanchal@Siddhis-MBP.attlocal.net>
2019-10-01 17:20:59 -0700

    decryption user flow done

---
+++ app/src/main/java/com/example/siddhipanchal/cipher/codedMessage.java
@@ -0,0 +1,21 @@
+package com.example.siddhipanchal.cipher;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class codedMessage extends AppCompatActivity {
+
+    //activity with the encrypted message
+    //will display CODE
+    //come here after encryption
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coded_message);
+        getSupportActionBar().hide();
+
+    }
+
+
+}
--- app/src/main/java/com/example/siddhipanchal/cipher/message.java
+++ app/src/main/java/com/example/siddhipanchal/cipher/message.java
@@ -1,24 +1,28 @@
 package com.example.siddhipanchal.cipher;
 
+
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class decryptActivity extends AppCompatActivity {
+//activity with the decrypted message printed on it
+//will be the original message
+//come here after hitting decrypt
 
+public class message extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.decrypt_activity);
+        setContentView(R.layout.message);
         getSupportActionBar().hide();
 
     }
 
-    public void goToMessage (View view){
-        Intent intent = new Intent(this, message.class);
+    public void goHome (View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
 }
