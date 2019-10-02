2030a0ea0d7fb15faa11baf33050d73ab9163afe
Siddhi Panchal <siddhipanchal@Siddhis-MBP.attlocal.net>
2019-10-02 13:33:34 -0700

    shared prefer

--- app/src/main/java/com/example/siddhipanchal/cipher/codedMessage.java
+++ app/src/main/java/com/example/siddhipanchal/cipher/codedMessage.java
@@ -1,13 +1,22 @@
 package com.example.siddhipanchal.cipher;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class codedMessage extends AppCompatActivity {
+
 
     //activity with the encrypted message
     //will display CODE
     //come here after encryption
+
+    public static String DEFAULT = "N/A";
+    TextView originalMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +24,22 @@ public class codedMessage extends AppCompatActivity {
         setContentView(R.layout.coded_message);
         getSupportActionBar().hide();
 
+        //initialization
+        originalMessage = findViewById(R.id.originalMessage);
+
+        //shared preference
+        SharedPreferences sharedPreferences = getSharedPreferences("mydata", Context.MODE_PRIVATE);
+
+        String OriginalMessage = sharedPreferences.getString("MessageReal", DEFAULT);
+
+        //set the new text
+        originalMessage.setText(OriginalMessage);
+
     }
 
 
+    public void goHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/siddhipanchal/cipher/encryptActivity.java
+++ app/src/main/java/com/example/siddhipanchal/cipher/encryptActivity.java
@@ -1,21 +1,37 @@
 package com.example.siddhipanchal.cipher;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class encryptActivity extends AppCompatActivity {
+
+    EditText messageReal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.encrypt_activity);
         getSupportActionBar().hide();
 
+        messageReal = findViewById(R.id.messageReal);
     }
 
 
     public void goToCodedMessage(View view) {
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("myData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("MessageReal", messageReal.getText().toString());
+        editor.commit();
+
+
+
         Intent intent = new Intent(this, codedMessage.class);
         startActivity(intent);
     }
