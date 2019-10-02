fd5a59a55804858a2bfd77280066dd0d018fa79d
Siddhi Panchal <siddhipanchal@Siddhis-MBP.attlocal.net>
2019-10-02 13:16:52 -0700

    routing

--- app/src/main/java/com/example/siddhipanchal/cipher/encryptActivity.java
+++ app/src/main/java/com/example/siddhipanchal/cipher/encryptActivity.java
@@ -1,7 +1,9 @@
 package com.example.siddhipanchal.cipher;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class encryptActivity extends AppCompatActivity {
     @Override
@@ -13,4 +15,8 @@ public class encryptActivity extends AppCompatActivity {
     }
 
 
+    public void goToCodedMessage(View view) {
+        Intent intent = new Intent(this, codedMessage.class);
+        startActivity(intent);
+    }
 }
